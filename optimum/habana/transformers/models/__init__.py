66645350947923d99bf9ed8877be5a9a96e0fdb0
Sun Choi <schoi@habana.ai>
2024-02-07 01:14:49 +0000

    falcon quantize linear

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -32,9 +32,11 @@ from .esm import (
     gaudi_rot_vec_mul,
 )
 from .falcon import (
+    GaudiFalconAttention,
     GaudiFalconForCausalLM,
+    GaudiFalconMLP,
     GaudiFalconModel,
-    gaudi_falcon_attention_forward,
+    #gaudi_falcon_attention_forward,
     gaudi_falcon_attention_split_heads,
     gaudi_falcon_decoder_layer_forward,
     gaudi_falcon_rotary_embedding_forward,
--- optimum/habana/transformers/models/falcon/modeling_falcon.py
+++ optimum/habana/transformers/models/falcon/modeling_falcon.py
@@ -26,14 +26,18 @@ except ImportError:
 
 
 import habana_frameworks.torch.core as htcore
+from torch import nn
 from torch.nn import CrossEntropyLoss
 from torch.nn import functional as F
 from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     CausalLMOutputWithCrossAttentions,
 )
+from transformers.models.falcon.configuration_falcon import FalconConfig
 from transformers.models.falcon.modeling_falcon import (
+    FalconAttention,
     FalconForCausalLM,
+    FalconMLP,
     FalconModel,
     build_alibi_tensor,
     dropout_add,
@@ -157,151 +161,170 @@ def gaudi_falcon_attention_split_heads(
         value = torch.index_select(fused_qkv, 2, index=torch.tensor([d2 + 1], device=fused_qkv.device))
         return query, key, value
 
+class GaudiFalconAttention(FalconAttention):
+    def __init__(self, config:FalconConfig):
+        super().__init__(config)
 
-def gaudi_falcon_attention_forward(
-    self,
-    hidden_states: torch.Tensor,
-    alibi: Optional[torch.Tensor],
-    attention_mask: torch.Tensor,
-    position_ids: Optional[torch.LongTensor] = None,
-    layer_past: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
-    head_mask: Optional[torch.Tensor] = None,
-    use_cache: bool = False,
-    output_attentions: bool = False,
-    token_idx: Optional[torch.Tensor] = None,
-):
-    """
-    Copied from FalconAttention.forward: https://github.com/huggingface/transformers/blob/main/src/transformers/models/falcon/modeling_falcon.py
-    The only differences are:
-    - add new args token_idx and position_ids
-    - replace F.scaled_dot_product_attention with Habana torch's version
-    """
-    fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
-    # 3 x [batch_size, seq_length, num_heads, head_dim]
-    (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
-
-    batch_size, query_length, _, _ = query_layer.shape
-
-    query_layer = query_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
-    key_layer = key_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
-    value_layer = value_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
-
-    past_kv_length = 0
-    seq_len = query_layer.shape[1]
-    if layer_past is not None:
-        if token_idx is not None:
-            # When token_idx is used,
-            # past_kv_length = 0
-            # static seq len = (input token len + max output token len)
-            seq_len = layer_past[0].shape[1]
+        if config.new_decoder_architecture:
+            qkv_out_dim = (config.num_kv_heads * 2 + config.num_attention_heads) * self.head_dim
+        elif config.multi_query:
+            qkv_out_dim = self.hidden_size + 2 * self.head_dim
         else:
-            past_kv_length = layer_past[0].shape[1]
+            qkv_out_dim = 3 * self.hidden_size
+        self.query_key_value = nn.Linear(self.hidden_size, qkv_out_dim, bias=config.bias) ##FalonLinear
+        self.dense = nn.Linear(self.hidden_size, self.hidden_size, bias=config.bias) ##FalconLinear
 
-    query_layer, key_layer = self.maybe_rotary(query_layer, key_layer, seq_len, position_ids, past_kv_length)
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        alibi: Optional[torch.Tensor],
+        attention_mask: torch.Tensor,
+        position_ids: Optional[torch.LongTensor] = None,
+        layer_past: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        use_cache: bool = False,
+        output_attentions: bool = False,
+        token_idx: Optional[torch.Tensor] = None,
+    ):
+        """
+        Copied from FalconAttention.forward: https://github.com/huggingface/transformers/blob/main/src/transformers/models/falcon/modeling_falcon.py
+        The only differences are:
+        - add new args token_idx and position_ids
+        - replace F.scaled_dot_product_attention with Habana torch's version
+        """
+        fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
+        # 3 x [batch_size, seq_length, num_heads, head_dim]
+        (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
 
-    if layer_past is not None:
-        past_key, past_value = layer_past
-        if token_idx is not None:
-            past_key.index_copy_(1, token_idx - 1, key_layer)
-            past_value.index_copy_(1, token_idx - 1, value_layer)
-            key_layer = past_key
-            value_layer = past_value
-        else:
-            # concatenate along seq_length dimension:
-            #  - key: [batch_size * self.num_heads, kv_length, head_dim]
-            #  - value: [batch_size * self.num_heads, kv_length, head_dim]
-            key_layer = torch.cat((past_key, key_layer), dim=1)
-            value_layer = torch.cat((past_value, value_layer), dim=1)
+        batch_size, query_length, _, _ = query_layer.shape
 
-    _, kv_length, _ = key_layer.shape
-    if use_cache:
-        present = (key_layer, value_layer)
-    else:
-        present = None
+        query_layer = query_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
+        key_layer = key_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
+        value_layer = value_layer.transpose(1, 2).reshape(-1, query_length, self.head_dim)
 
-    float_min = torch.finfo(query_layer.dtype).min
-    attention_mask_float = (attention_mask * 1.0).masked_fill(attention_mask, float_min).to(query_layer.dtype)
-
-    query_layer_ = query_layer.reshape(batch_size, -1, query_length, self.head_dim)
-    key_layer_ = key_layer.reshape(batch_size, -1, seq_len, self.head_dim)
-    value_layer_ = value_layer.reshape(batch_size, -1, seq_len, self.head_dim)
-
-    if alibi is None:
-        if output_attentions:
-            attention_scores = query_layer_ @ key_layer_.transpose(-1, -2)
-            attention_scores /= math.sqrt(self.head_dim)
-
-            attention_scores = F.softmax(attention_scores + attention_mask_float, dim=-1, dtype=hidden_states.dtype)
-            attn_output = attention_scores @ value_layer_
-        else:
-            if FusedSDPA:
-                with sdp_kernel(enable_recompute=False) if SDPContext else contextlib.nullcontext():
-                    attn_output = FusedSDPA.apply(
-                        query_layer_, key_layer_, value_layer_, attention_mask_float, 0.0, False
-                    )
+        past_kv_length = 0
+        seq_len = query_layer.shape[1]
+        if layer_past is not None:
+            if token_idx is not None:
+                # When token_idx is used,
+                # past_kv_length = 0
+                # static seq len = (input token len + max output token len)
+                seq_len = layer_past[0].shape[1]
             else:
-                # Workaround util scaled_dot_product_attention support broadcast.
-                if self.training is True and query_layer_.shape != key_layer_.shape:
-                    key_layer_ = torch.broadcast_to(key_layer_, query_layer_.shape)
-                    value_layer_ = torch.broadcast_to(value_layer_, query_layer_.shape)
-                attn_output = F.scaled_dot_product_attention(
-                    query_layer_, key_layer_, value_layer_, attention_mask_float, 0.0, is_causal=False
-                )
-            # Performance improvement for HPU
-            if self.training is True and htcore:
-                htcore.mark_step()
-            attention_scores = None
+                past_kv_length = layer_past[0].shape[1]
 
-        attn_output = attn_output.view(batch_size, -1, query_length, self.head_dim)
-        attn_output = attn_output.permute(0, 2, 1, 3)
-        attn_output = attn_output.reshape(batch_size, query_length, -1)
+        query_layer, key_layer = self.maybe_rotary(query_layer, key_layer, seq_len, position_ids, past_kv_length)
 
-        output_tensor = self.dense(attn_output)
+        if layer_past is not None:
+            past_key, past_value = layer_past
+            if token_idx is not None:
+                past_key.index_copy_(1, token_idx - 1, key_layer)
+                past_value.index_copy_(1, token_idx - 1, value_layer)
+                key_layer = past_key
+                value_layer = past_value
+            else:
+                # concatenate along seq_length dimension:
+                #  - key: [batch_size * self.num_heads, kv_length, head_dim]
+                #  - value: [batch_size * self.num_heads, kv_length, head_dim]
+                key_layer = torch.cat((past_key, key_layer), dim=1)
+                value_layer = torch.cat((past_value, value_layer), dim=1)
 
-        if output_attentions:
-            return output_tensor, present, attention_scores
+        _, kv_length, _ = key_layer.shape
+        if use_cache:
+            present = (key_layer, value_layer)
         else:
-            return output_tensor, present
+            present = None
 
-    else:
-        matmul_result = query_layer_ @ key_layer_.transpose(-1, -2)
+        float_min = torch.finfo(query_layer.dtype).min
+        attention_mask_float = (attention_mask * 1.0).masked_fill(attention_mask, float_min).to(query_layer.dtype)
 
-        # change view to [batch_size, num_heads, q_length, kv_length]
-        attention_scores = matmul_result.view(batch_size, self.num_heads, query_length, kv_length)
+        query_layer_ = query_layer.reshape(batch_size, -1, query_length, self.head_dim)
+        key_layer_ = key_layer.reshape(batch_size, -1, seq_len, self.head_dim)
+        value_layer_ = value_layer.reshape(batch_size, -1, seq_len, self.head_dim)
 
-        # cast attention scores to fp32, compute scaled softmax and cast back to initial dtype - [batch_size, num_heads, q_length, kv_length]
-        input_dtype = attention_scores.dtype
-        # `float16` has a minimum value of -65504.0, whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
-        if input_dtype == torch.float16 or input_dtype == torch.bfloat16:
-            attention_scores = attention_scores.to(torch.float32)
-        # Matt (HF) note: We could possibly use F.scaled_dot_product_attention here too, by
-        # adding (alibi * self.inv_norm_factor) to attention_mask_float. I think this would be mathematically
-        # equivalent and more performant, but there might be a numerical difference. If you're reading this
-        # and you'd like to experiment and maybe file a PR, feel free!
-        attention_logits = attention_scores + alibi.view(batch_size, self.num_heads, 1, -1)
-        attention_logits *= self.inv_norm_factor
-        attention_probs = F.softmax(attention_logits + attention_mask_float, dim=-1, dtype=hidden_states.dtype)
-        # [batch_size, num_heads, q_length, kv_length]
-        attention_probs = self.attention_dropout(attention_probs)
+        if alibi is None:
+            if output_attentions:
+                attention_scores = query_layer_ @ key_layer_.transpose(-1, -2)
+                attention_scores /= math.sqrt(self.head_dim)
 
-        if head_mask is not None:
-            attention_probs = attention_probs * head_mask
+                attention_scores = F.softmax(attention_scores + attention_mask_float, dim=-1, dtype=hidden_states.dtype)
+                attn_output = attention_scores @ value_layer_
+            else:
+                if FusedSDPA:
+                    with sdp_kernel(enable_recompute=False) if SDPContext else contextlib.nullcontext():
+                        attn_output = FusedSDPA.apply(
+                            query_layer_, key_layer_, value_layer_, attention_mask_float, 0.0, False
+                        )
+                else:
+                    # Workaround util scaled_dot_product_attention support broadcast.
+                    if self.training is True and query_layer_.shape != key_layer_.shape:
+                        key_layer_ = torch.broadcast_to(key_layer_, query_layer_.shape)
+                        value_layer_ = torch.broadcast_to(value_layer_, query_layer_.shape)
+                    attn_output = F.scaled_dot_product_attention(
+                        query_layer_, key_layer_, value_layer_, attention_mask_float, 0.0, is_causal=False
+                    )
+                # Performance improvement for HPU
+                if self.training is True and htcore:
+                    htcore.mark_step()
+                attention_scores = None
 
-        # change view [batch_size, num_heads, q_length, kv_length]
-        attention_probs_reshaped = attention_probs.view(batch_size, self.num_heads, query_length, kv_length)
+            attn_output = attn_output.view(batch_size, -1, query_length, self.head_dim)
+            attn_output = attn_output.permute(0, 2, 1, 3)
+            attn_output = attn_output.reshape(batch_size, query_length, -1)
 
-        # matmul: [batch_size * num_heads, q_length, head_dim]
-        context_layer = (attention_probs_reshaped @ value_layer_).flatten(0, 1)
+            output_tensor = self.dense(attn_output)
 
-        # change view [batch_size, q_length, num_heads * head_dim]
-        context_layer = self._merge_heads(context_layer)
+            if output_attentions:
+                return output_tensor, present, attention_scores
+            else:
+                return output_tensor, present
 
-        output_tensor = self.dense(context_layer)
-
-        if output_attentions:
-            return output_tensor, present, attention_probs
         else:
-            return output_tensor, present
+            matmul_result = query_layer_ @ key_layer_.transpose(-1, -2)
+
+            # change view to [batch_size, num_heads, q_length, kv_length]
+            attention_scores = matmul_result.view(batch_size, self.num_heads, query_length, kv_length)
+
+            # cast attention scores to fp32, compute scaled softmax and cast back to initial dtype - [batch_size, num_heads, q_length, kv_length]
+            input_dtype = attention_scores.dtype
+            # `float16` has a minimum value of -65504.0, whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
+            if input_dtype == torch.float16 or input_dtype == torch.bfloat16:
+                attention_scores = attention_scores.to(torch.float32)
+            # Matt (HF) note: We could possibly use F.scaled_dot_product_attention here too, by
+            # adding (alibi * self.inv_norm_factor) to attention_mask_float. I think this would be mathematically
+            # equivalent and more performant, but there might be a numerical difference. If you're reading this
+            # and you'd like to experiment and maybe file a PR, feel free!
+            attention_logits = attention_scores + alibi.view(batch_size, self.num_heads, 1, -1)
+            attention_logits *= self.inv_norm_factor
+            attention_probs = F.softmax(attention_logits + attention_mask_float, dim=-1, dtype=hidden_states.dtype)
+            # [batch_size, num_heads, q_length, kv_length]
+            attention_probs = self.attention_dropout(attention_probs)
+
+            if head_mask is not None:
+                attention_probs = attention_probs * head_mask
+
+            # change view [batch_size, num_heads, q_length, kv_length]
+            attention_probs_reshaped = attention_probs.view(batch_size, self.num_heads, query_length, kv_length)
+
+            # matmul: [batch_size * num_heads, q_length, head_dim]
+            context_layer = (attention_probs_reshaped @ value_layer_).flatten(0, 1)
+
+            # change view [batch_size, q_length, num_heads * head_dim]
+            context_layer = self._merge_heads(context_layer)
+
+            output_tensor = self.dense(context_layer)
+
+            if output_attentions:
+                return output_tensor, present, attention_probs
+            else:
+                return output_tensor, present
+
+class GaudiFalconMLP(FalconMLP):
+    def __init__(self, config: FalconConfig):
+        super().__init__(config)
+        hidden_size = config.hidden_size
+        self.dense_h_to_4h = nn.Linear(hidden_size, 4 * hidden_size, bias=config.bias) #FalconLinear
+        self.dense_4h_to_h = nn.Linear(4 * hidden_size, hidden_size, bias=config.bias) #FalconLinear
 
 
 def gaudi_falcon_decoder_layer_forward(

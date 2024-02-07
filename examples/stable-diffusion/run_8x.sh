4e0dd8c282220234e174f7046cc2f23c73aeaa91
Shiv Kaul <skaul@habana.ai>
2024-02-06 17:17:46 -0800

    Create run_8x.sh

---
+++ examples/stable-diffusion/run_8x.sh
@@ -0,0 +1,23 @@
+python ../gaudi_spawn.py --world_size 8 --use_mpi train_text_to_image_sdxl.py \
+  --pretrained_model_name_or_path stabilityai/stable-diffusion-xl-base-1.0 \
+  --pretrained_vae_model_name_or_path stabilityai/sdxl-vae \
+  --dataset_name lambdalabs/pokemon-blip-captions \
+  --resolution 512 \
+  --center_crop \
+  --random_flip \
+  --proportion_empty_prompts=0.2 \
+  --train_batch_size 16 \
+  --max_train_steps 2500 \
+  --learning_rate 1e-05 \
+  --max_grad_norm 1 \
+  --lr_scheduler constant \
+  --lr_warmup_steps 0 \
+  --output_dir sdxl-pokemon-model \
+  --gaudi_config_name Habana/stable-diffusion \
+  --throughput_warmup_steps 3 \
+  --bf16 \
+  --validation_prompt="a horse running on the beach during sunset" \
+  --validation_epochs 48 \
+  --use_hpu_graphs \
+  --checkpointing_steps 2500 \
+  --cache_dir /root/software/data/pytorch/huggingface/sdxl 2>&1 | tee log_8x_bs16.txt

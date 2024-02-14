21eb679db29709912e0e9dcd350a57865ff700bc
Yeonsil Yoon <yyoon@habana.ai>
2024-02-13 16:36:14 -0800

    Update run_8x.sh

--- examples/stable-diffusion/run_8x.sh
+++ examples/stable-diffusion/run_8x.sh
@@ -2,7 +2,7 @@ python ../gaudi_spawn.py --world_size 8 --use_mpi train_text_to_image_sdxl.py \
   --pretrained_model_name_or_path stabilityai/stable-diffusion-xl-base-1.0 \
   --pretrained_vae_model_name_or_path stabilityai/sdxl-vae \
   --dataset_name lambdalabs/pokemon-blip-captions \
-  --resolution 512 \
+  --resolution 1024 \
   --center_crop \
   --random_flip \
   --proportion_empty_prompts=0.2 \
@@ -15,9 +15,10 @@ python ../gaudi_spawn.py --world_size 8 --use_mpi train_text_to_image_sdxl.py \
   --output_dir sdxl-pokemon-model \
   --gaudi_config_name Habana/stable-diffusion \
   --throughput_warmup_steps 3 \
+  --dataloader_num_workers 8 \
   --bf16 \
-  --validation_prompt="a horse running on the beach during sunset" \
+  --use_hpu_graphs_for_inference \
+  --validation_prompt="a robotic cat with wings" \
   --validation_epochs 48 \
-  --use_hpu_graphs \
   --checkpointing_steps 2500 \
   --cache_dir /root/software/data/pytorch/huggingface/sdxl 2>&1 | tee log_8x_bs16.txt

3603cc68419f58fe2e553a0438b7723a31924e08
Vivek <vgoel@habana.ai>
2024-02-18 13:22:41 +0200

    Wav2Vec-ASR hpu graphs initial commit

--- optimum/habana/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ optimum/habana/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -17,11 +17,16 @@
 from typing import Optional, Tuple, Union
 
 import torch
+from habana_frameworks.torch.hpex.kernels import CTCLoss
 from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
 from transformers.modeling_outputs import (
     BaseModelOutput,
+    CausalLMOutput,
     Wav2Vec2BaseModelOutput,
 )
+
+
+ctc_loss_fwd = CTCLoss.apply
 
 
 def _gaudi_wav2vec2_compute_mask_indices(
@@ -122,8 +127,9 @@ def _gaudi_wav2vec2_compute_mask_indices(
     spec_aug_mask_idxs = spec_aug_mask_idxs + offsets
 
     # ensure that we cannot have indices larger than sequence_length
-    if spec_aug_mask_idxs.max() > sequence_length - 1:
-        spec_aug_mask_idxs[spec_aug_mask_idxs > sequence_length - 1] = sequence_length - 1
+    mask = (spec_aug_mask_idxs > sequence_length - 1) * (spec_aug_mask_idxs.max() > sequence_length - 1)
+    inverse_mask = torch.bitwise_not(mask)
+    spec_aug_mask_idxs = spec_aug_mask_idxs * inverse_mask + (sequence_length - 1) * mask
 
     # scatter indices to mask
     spec_aug_mask.scatter_(-1, spec_aug_mask_idxs, 1)
@@ -361,3 +367,68 @@ def gaudi_wav2vec2_encoder_forward(
         hidden_states=all_hidden_states,
         attentions=all_self_attentions,
     )
+
+
+_HIDDEN_STATES_START_POSITION = 2
+
+
+def gaudi_wav2vec2forctc_forward(
+    self,
+    input_values: Optional[torch.Tensor],
+    attention_mask: Optional[torch.Tensor] = None,
+    output_attentions: Optional[bool] = None,
+    output_hidden_states: Optional[bool] = None,
+    return_dict: Optional[bool] = None,
+    labels: Optional[torch.Tensor] = None,
+) -> Union[Tuple, CausalLMOutput]:
+    r"""
+    labels (`torch.LongTensor` of shape `(batch_size, target_length)`, *optional*):
+        Labels for connectionist temporal classification. Note that `target_length` has to be smaller or equal to
+        the sequence length of the output logits. Indices are selected in `[-100, 0, ..., config.vocab_size - 1]`.
+        All labels set to `-100` are ignored (masked), the loss is only computed for labels in `[0, ...,
+        config.vocab_size - 1]`.
+    """
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+    outputs = self.wav2vec2(
+        input_values,
+        attention_mask=attention_mask,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
+    )
+    hidden_states = outputs[0]
+    hidden_states = self.dropout(hidden_states)
+    logits = self.lm_head(hidden_states)
+    loss = None
+    if labels is not None:
+        if labels.max() >= self.config.vocab_size:
+            raise ValueError(f"Label values must be <= vocab_size: {self.config.vocab_size}")
+        # retrieve loss input_lengths from attention_mask
+        attention_mask = (
+            attention_mask
+            if attention_mask is not None
+            else torch.ones_like(input_values, dtype=torch.long, device="hpu")
+        )
+        input_lengths = self._get_feat_extract_output_lengths(attention_mask.sum(-1)).to(torch.long)
+        # assuming that padded tokens are filled with -100
+        # when not being attended to
+        labels_mask = labels >= 0
+        target_lengths = labels_mask.sum(-1)
+        flattened_targets = labels  # labels.masked_select(labels_mask)
+        # ctc_loss doesn't support fp16
+        log_probs = torch.nn.functional.log_softmax(logits, dim=-1, dtype=torch.float32).transpose(0, 1)
+        with torch.backends.cudnn.flags(enabled=False):
+            loss = ctc_loss_fwd(
+                log_probs,
+                flattened_targets,
+                input_lengths,
+                target_lengths,
+                self.config.pad_token_id,
+                self.config.ctc_loss_reduction,
+                self.config.ctc_zero_infinity,
+            )
+
+    if not return_dict:
+        output = (logits,) + outputs[_HIDDEN_STATES_START_POSITION:]
+        return ((loss,) + output) if loss is not None else output
+    return CausalLMOutput(loss=loss, logits=logits, hidden_states=outputs.hidden_states, attentions=outputs.attentions)

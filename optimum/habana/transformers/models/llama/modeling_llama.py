7059c731e02deac8812b951e00a902bcf0264364
Shiv Kaul <skaul@habana.ai>
2024-04-01 12:28:14 -0700

    Update modeling_llama.py

--- optimum/habana/transformers/models/llama/modeling_llama.py
+++ optimum/habana/transformers/models/llama/modeling_llama.py
@@ -975,9 +975,9 @@ def apply_customized_rope(q, k, cos, sin, position_ids, use_fused_rope=True):
     if q.device.type == "hpu" and has_fused_rope and use_fused_rope:
         # TODO: remove `.clone()` when SynapseAI v1.15 is released
         return FusedRoPE.apply(
-            q, cos.unsqueeze(0).unsqueeze(0).clone(), sin.unsqueeze(0).unsqueeze(0).clone(), position_ids
+            q.to(torch.bfloat16), cos.unsqueeze(0).unsqueeze(0).clone(), sin.unsqueeze(0).unsqueeze(0).clone(), position_ids
         ), FusedRoPE.apply(
-            k, cos.unsqueeze(0).unsqueeze(0).clone(), sin.unsqueeze(0).unsqueeze(0).clone(), position_ids
+            k.to(torch.bfloat16), cos.unsqueeze(0).unsqueeze(0).clone(), sin.unsqueeze(0).unsqueeze(0).clone(), position_ids
         )
     else:
         return apply_rotary_pos_emb(q, k, cos, sin, position_ids)

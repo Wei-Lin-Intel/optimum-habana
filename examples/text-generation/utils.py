384ea1539ab84fb1702bf3d4b1dba21486f9681f
Danny Semiat <dsemiat@habana.ai>
2024-05-21 16:16:24 +0300

    Removed call to preprocessing (found a better solution on AutoGPTQ)

--- examples/text-generation/utils.py
+++ examples/text-generation/utils.py
@@ -170,12 +170,6 @@ def setup_model(args, model_dtype, model_kwargs, logger):
         from transformers import GPTQConfig
         quantization_config = GPTQConfig(bits=4, disable_exllama=True)
         model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, torch_dtype=model_dtype, quantization_config=quantization_config, **model_kwargs)
-        def run_preprocessing(model):
-            for name, mod in model.named_children():
-                if hasattr(mod, "preprocessing"):
-                    mod.preprocessing()
-                run_preprocessing(mod)
-        run_preprocessing(model)
     else:
         model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, torch_dtype=model_dtype, **model_kwargs)
     if args.quant_config:

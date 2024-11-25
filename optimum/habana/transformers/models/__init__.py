2530c8f0caaf3126b840fafc9252f3a2442d170b
Krzysztof Wiśniewski <krzysztof2.wisniewski@intel.com>
2024-11-25 10:52:56 +0100

    Add dynamic Moe to Mixtral (#31)
    * Add DynamicMoE support for Mixtral
    
    ---------
    
    Co-authored-by: Adam Stachowicz <105052242+astachowiczhabana@users.noreply.github.com>

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -150,6 +150,7 @@ from .mistral import (
     gaudi_mistral_rmsnorm_forward,
 )
 from .mixtral import (
+    GaudiDynamicMoeBlock,
     GaudiMixtralAttention,
     GaudiMixtralDecoderLayer,
     GaudiMixtralForCausalLM,

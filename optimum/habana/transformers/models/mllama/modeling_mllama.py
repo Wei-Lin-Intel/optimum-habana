6cadb1b38f50f8bd73dd6c44993c7b7301be1389
Adam Stachowicz <105052242+astachowiczhabana@users.noreply.github.com>
2025-04-01 10:40:30 +0200

    Revert " "num_vision_tokens = 1601" that creates unaligned FCD output - causing partial writes. (#205)" (#218)
    This reverts commit 765f800cb4fac3a40c61bc4d5fb2d3d5f29cc3f4.

--- optimum/habana/transformers/models/mllama/modeling_mllama.py
+++ optimum/habana/transformers/models/mllama/modeling_mllama.py
@@ -84,11 +84,14 @@ def _prepare_cross_attention_mask(
     The only differences are:
         - if there's pading in cross_attention_mask in the right. do not masked it, or else it will impact softmax in crossattention
     """
+<<<<<<< HEAD
     # reshape so it can be used by attn module
     batch_size, text_total_length, *_ = cross_attention_mask.shape
     cross_attention_mask = cross_attention_mask.repeat_interleave(num_vision_tokens, dim=3)
     cross_attention_mask = cross_attention_mask.view(batch_size, text_total_length, -1)
     cross_attention_mask = cross_attention_mask.unsqueeze(1)
+=======
+>>>>>>> f7c7e26d (Revert " "num_vision_tokens = 1601" that creates unaligned FCD output - causing partial writes. (#205)" (#218))
 
     # invert the mask
     inverted_cross_attn_mask = (1.0 - cross_attention_mask).to(dtype)
@@ -108,6 +111,12 @@ def _prepare_cross_attention_mask(
         cross_attention_mask *= full_text_row_masked_out_mask2
     else:
         cross_attention_mask *= full_text_row_masked_out_mask
+
+    # reshape so it can be used by attn module
+    batch_size, text_total_length, *_ = cross_attention_mask.shape
+    cross_attention_mask = cross_attention_mask.repeat_interleave(num_vision_tokens, dim=3)
+    cross_attention_mask = cross_attention_mask.view(batch_size, text_total_length, -1)
+    cross_attention_mask = cross_attention_mask.unsqueeze(1)
 
     return cross_attention_mask, full_text_row_masked_out_mask
 

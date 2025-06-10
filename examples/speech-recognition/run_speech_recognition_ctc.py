e2c893d72486585971b3de4c7891f81f263d360a
gplutop7 <gplutopx@habana.ai>
2025-06-10 19:46:37 +0300

    uncomment trust_remote_code

--- examples/speech-recognition/run_speech_recognition_ctc.py
+++ examples/speech-recognition/run_speech_recognition_ctc.py
@@ -497,7 +497,7 @@ def main():
         "name": data_args.dataset_config_name,
         "split": data_args.train_split_name,
         #"token": data_args.token,
-        #"trust_remote_code": data_args.trust_remote_code,
+        "trust_remote_code": data_args.trust_remote_code,
     }
     if data_args.dataset_dir is not None:
         load_dataset_kwargs["data_dir"] = data_args.dataset_dir

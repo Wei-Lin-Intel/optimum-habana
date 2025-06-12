6b762c2d89360a6950f157ff50f8ee017456da1f
gplutop7 <gplutopx@habana.ai>
2025-06-12 15:55:51 +0300

    revert

--- examples/speech-recognition/run_speech_recognition_ctc.py
+++ examples/speech-recognition/run_speech_recognition_ctc.py
@@ -1,28 +1,15 @@
-import os
 from datasets import load_dataset
 
-DATASET_NAME = "librispeech_asr"
+DATASET_SCRIPT = "/software/data/librispeech_asr/librispeech_asr.py"
+DATA_DIR = "/software/data/librispeech_asr/LibriSpeech"
 CONFIG_NAME = "clean"
 SPLIT = "train.100"
-DATA_DIR = "/software/data/librispeech_asr/LibriSpeech"
-SCRIPT_PATH = "/root/librispeech_asr.py"
 
-# Pobierz tylko plik datasetu .py (jeśli nie istnieje)
-if not os.path.exists(SCRIPT_PATH):
-    print(f"Downloading dataset script to {SCRIPT_PATH}...")
-    os.system(
-        f"wget https://raw.githubusercontent.com/huggingface/datasets/main/datasets/librispeech_asr/librispeech_asr.py -O {SCRIPT_PATH}"
-    )
-
-# Użycie lokalnego datasetu
-print("Loading dataset using local script...")
 dataset = load_dataset(
-    path=SCRIPT_PATH,
+    path=DATASET_SCRIPT,
     name=CONFIG_NAME,
     split=SPLIT,
-    data_dir=DATA_DIR,
-    trust_remote_code=True,
+    data_dir=DATA_DIR
 )
 
-print("Dataset loaded successfully.")
 print(dataset)
